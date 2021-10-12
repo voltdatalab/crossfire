65ae1fedd435716be6b311c481f0409577f55ac5
Felipe Barros <felipe.b4rros@gmail.com>
2021-10-11 23:59:44 -0300

    alter try except get_token_fogocruzado function

--- Python/fogocruzado_utils.py
+++ Python/fogocruzado_utils.py
@@ -1,6 +1,5 @@
 import json
 import os
-import sys
 
 import requests
 
@@ -21,11 +20,12 @@ def get_token_fogocruzado():
             data={'email': os.getenv("FOGO_CRUZADO_EMAIL"),
                   'password': os.getenv("FOGO_CRUZADO_PASSWORD")}
         )
-        access_fogocruzado = json.loads(post_fogocruzado.content).get('access_token')
-        accesstoken_fogocruzado = f"Bearer {access_fogocruzado}"
-        os.environ["FOGO_CRUZADO_API_TOKEN"] = accesstoken_fogocruzado
         post_fogocruzado.raise_for_status()
 
     except requests.exceptions.HTTPError:
         raise SystemExit(
             "These credentials do not correspond to Fogo Cruzado's records. \nPlease check your e-mail and password or access https://api.fogocruzado.org.br/register to register.")
+
+    access_fogocruzado = json.loads(post_fogocruzado.content).get('access_token')
+    accesstoken_fogocruzado = f"Bearer {access_fogocruzado}"
+    os.environ["FOGO_CRUZADO_API_TOKEN"] = accesstoken_fogocruzado
