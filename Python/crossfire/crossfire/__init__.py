3326177c711323220785b863c538b872269852ea
Eduardo Vicente Gonçalves <4732915+cuducos@users.noreply.github.com>
2023-09-08 10:56:29 -0400

    Fixes modules name conflicts

--- Python/crossfire/crossfire/__init__.py
+++ Python/crossfire/crossfire/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "0.1.0"
 
-from crossfire.fogocruzado_signin import fogocruzado_signin  # noqa
-from crossfire.get_fogocruzado import get_fogocruzado  # noqa
+from crossfire.signin import fogocruzado_signin  # noqa
+from crossfire.load_data import get_fogocruzado  # noqa
